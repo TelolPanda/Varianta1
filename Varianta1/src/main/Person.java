6b15d5f0ab39d1f9148b69d74ec9741d13e930b8
crankiexz <78876584+crankiexz@users.noreply.github.com>
2021-02-11 22:05:51 +0200

    Person.java

--- Varianta1/src/main/Person.java
+++ Varianta1/src/main/Person.java
@@ -1,5 +1,48 @@
 package main;
 
-public class Person {
-
+public class Person
+{
+	String fullName;
+	String dateOfBirth;
+	String gender;
+	String phoneNumber;
+	
+	
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	public String getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(String dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+	@Override
+	public String toString()
+	{
+		String info = "";
+		info+="Full names is :"+ fullName+ "\n";
+		info+="Date of birth is: "+dateOfBirth+" \n" ;
+		info+="Gender is : "+gender+ "\n";
+		info+="Phone Number is :" +phoneNumber+ " \n";
+		return info;
+	}
+	
+	
 }
