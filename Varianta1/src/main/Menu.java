fc323a474c9a2a20faed3278b380e6e990bdf35c
adelina93 <78814937+adelina93@users.noreply.github.com>
2021-02-15 21:32:34 +0200

    Add files via upload

---
+++ Varianta1/src/main/Menu.java
@@ -0,0 +1,41 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Menu 
+{
+
+	List<String> m = new ArrayList<>();
+	
+	Scanner sc = new Scanner(System.in);
+	
+	int id;
+	
+	public int add(String option)
+	{		
+		id ++;
+		
+		m.add( id + ". " + option );
+		
+		return id;
+	}
+	
+	public void show()
+	{
+		for (int i=0; i<m.size(); i++)
+		System.out.println(m.get(i));
+	}
+		
+	
+	
+	public int getOption()
+	{
+		int op;
+		
+		op = sc.nextInt();
+		
+		return op;
+	}
+}
