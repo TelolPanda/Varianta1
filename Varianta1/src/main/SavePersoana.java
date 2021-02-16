800d5cf64840fc831c92bbe5b66b28daaed958b7
Tudor <tudor.gioroc@gmail.com>
2021-02-16 20:29:27 +0200

    mi-am pus numele in ambele clase

--- Varianta1/src/main/SavePersoana.java
+++ Varianta1/src/main/SavePersoana.java
@@ -4,7 +4,10 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
-
+/*
+ * Tudor Andrei Gioroc
+ * 2021-02-16
+ */
 public class SavePersoana 
 {
 	
