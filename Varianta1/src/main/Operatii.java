786e7d76d5040a6813d1f43d76c6b7f2968e9366
stefanenea <stefanenea2005@gmail.com>
2021-02-16 21:32:06 +0200

    Am citit review-ul si am facut modificari

--- Varianta1/src/main/Operatii.java
+++ Varianta1/src/main/Operatii.java
@@ -8,57 +8,58 @@ import java.util.Scanner;
  */
 public class Operatii 
 {	
-	static Person p1 = new Person();
+	
 	static ArrayList<String> ar = new ArrayList();
 	static Scanner sc = new Scanner (System.in);
 	
 	public static Person  angajeaza()
 	{
+		Person p = new Person();
 		System.out.println("Introduceti numele");
-		p1.setFullName(sc.nextLine());
+		p.setFullName(sc.nextLine());
 		
 		System.out.println("Introduceti sexul");
-		p1.setGender(sc.nextLine());
+		p.setGender(sc.nextLine());
 		
 		System.out.println("Introduceti data nasterii");
-		p1.setDateOfBirth(sc.nextLine());
+		p.setDateOfBirth(sc.nextLine());
 		
 		System.out.println("Introduceti numarul de telefon");
-		p1.setPhoneNumber(sc.nextLine());
+		p.setPhoneNumber(sc.nextLine());
 		
-		return p1;
+		return p;
 		
 	}
 	
-	public void concediaza()
-	{
-		String employee;
-		System.out.println("Alege angajatul pe care vrei sa il concediezi");
-		employee = sc.nextLine();
-		if(ar.contains(employee))
-		{
-			ar.remove(employee);
-			System.out.println("Angajatul " + employee + " a fost concediat");
-		}
-		else
-		{
-			System.out.println("Aceasta persoana nu este angajata");
-			System.out.println("Verificati daca ati introdus gresit numele");
-		}
-	}
-	public void actualizeaza()
-	{
-		String informatie;
-		System.out.println("Alegeti informatia pe care vreti sa o schimbati");
-		informatie = sc.nextLine();
-		//ar.remove(informatie);
-		String informatieNoua;
-		System.out.println("Introduceti noua informatie");
-		informatieNoua = sc.nextLine();
+//	public void concediaza()
+//	{
+		//String employee;
+		//System.out.println("Alege angajatul pe care vrei sa il concediezi");
+	//	employee = sc.nextLine();
+		//if(ar.contains(employee))
+	//	{
+	//		ar.remove(employee);
+	//		System.out.println("Angajatul " + employee + " a fost concediat");
+	//	}
+		//else
+	//	{
+		//	System.out.println("Aceasta persoana nu este angajata");
+		//	System.out.println("Verificati daca ati introdus gresit numele");
+		//}
+//	}
+	//public void actualizeaza()
+	//{
+	//	String informatie;
+	//	System.out.println("Alegeti informatia pe care vreti sa o schimbati");
+	//	informatie = sc.nextLine();
+	//	//ar.remove(informatie);
+	//	String informatieNoua;
+	//	System.out.println("Introduceti noua informatie");
+		//informatieNoua = sc.nextLine();
 		//ar.add(informatieNoua);
 		
 		
-	}
+	//}
 	public void show()
 	{
 		System.out.println("Lista angajatilor este " + ar.toString().replace("[", "").replace("]", ""));
